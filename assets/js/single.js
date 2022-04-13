288be04ccb00a815033f7820c5232a70559421ca
hkagei <k_dawg_1022@hotmail.com>
2022-04-12 19:09:08 -0600

    redo single.js file

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,63 +1,53 @@
-var getRepoIssues = function(repo) {
-    console.log(repo);
-  };
-  
-var displayIssues = function(issues) {
-
-};
 var issueContainerEl = document.querySelector("#issues-container");
-var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-  getRepoIssues("facebook/react"){
-      var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
-      fetch(apiUrl).then(function(response) {
-        if (issues.length === 0) {
-            issueContainerEl.textContent = "This repo has no open issues!";
-            return;
-          }
-
-        
-        // request was successful
+    fetch(apiUrl).then(function(response) {
+        //request was successful
         if (response.ok) {
             response.json().then(function(data) {
+                // pass response data to dom function
                 displayIssues(data);
-            
-                // check if api has paginated issues
-                if (response.headers.get("Link")) {
-                  console.log("repo has more than 30 issues");
-                }
-              });
-            }
-            
-                    // create a link element to take users to the issue on github
-                    // create span to hold issue title
-var titleEl = document.createElement("span");
-titleEl.textContent = issues[i].title;
-
-// append to container
-issueEl.appendChild(titleEl);
-
-// create a type element
-var typeEl = document.createElement("span");
-
-// check if issue is an actual issue or a pull request
-if (issues[i].pull_request) {
-  typeEl.textContent = "(Pull request)";
-} else {
-  typeEl.textContent = "(Issue)";
-}
-
-// append to container
-issueEl.appendChild(typeEl);
-
-                  }
-                  
-            }
-          });
+            });
         }
         else {
-          alert("There was a problem with your request!");
+            alert("There was a problem with your request!");
         }
-      });
+    });
+    console.log(repo);
+  };
+
+  var displayIssues = function(issues) {
+
+    for (var i = 0; i < issues.length; i++) {
+        // creates a link element to take users to the issue on github
+         var issueEl = document.createElement("a");
+         issueEl.classList = "list-item flex-row justify-space-between align-center";
+
+         issueEl.setAttribute("href", issues[i].htm_url);
+
+         issueEl.setAttribute("target", "_blank");
+
+         // creates span to hold issue title
+         var titleEl = document.createElement("span");
+         titleEl.textContent = issues[i].title;
+
+         // append to container
+         issueEl.appendChild(titleEl);
+
+         // create a type element
+         var typeEl = document.createElement("span");
+
+         // check if an issue is an actual issue or a pull request
+         if (issues[i].pull_request) {
+             typeEl.textContent = "(Pull request)";
+         } else {
+             typeEl.textContent = "(Issue)";
+         }
+         issueContainerEl.appendChild(issueEl);
+    }
+
+};
+  
+  getRepoIssues("facebook/react");
